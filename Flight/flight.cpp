4e20c280cd786fb014247eebd2db87512f9ff04d
Siddhant Rakibe <102241647+KCREEK@users.noreply.github.com>
2023-06-05 01:55:23 +0530

    distance by cities

--- Flight/flight.cpp
+++ Flight/flight.cpp
@@ -4,8 +4,60 @@
    represented by airport name or name of the city.
 */
 
+#include <bits/stdc++.h>
+#include <ctime>
 
+using namespace std;
 
+//initializing an adjacency matrix
+int adj_mat[50][50]={0,0};
+
+int main(){
+  cout<<"Enter no. of cities: ";
+  int n;
+  cin>>n;
+  string cities[n];
+
+//Taking input for the number of cities from n
+  for(int i=0;i<n;i++){
+    cout<<"Enter city"<<i+1<<"(Airport code): ";
+    cin>>cities[i];
+  }
+
+//Displaying the cities that we've taken input of
+cout<<"YOUR CITIES ARE"<<endl;
+for(int i=0;i<n;i++){
+  cout<<"\nCity"<<i+1<<": "<<cities[i];
+}
+
+for(int i=0;i<n;i++){
+  for(int j=i+1;j<n;j++){
+    cout<<"\nEnter distance between "<<cities[i]<<" and "<<cities[j]<<"; ";
+    cin>>adj_mat[i][j];
+    adj_mat[j][i]=adj_mat[i][j];
+  }
+}
+
+for(int i=0;i<n;i++){
+  cout<<"\t"<<"cities[i]"<<"\t";
+}
+
+//displaying the matrix of the distance traveled by the flights here
+for(int i=0;i<n;i++){
+  cout<<"\n"<<cities[i];
+  for(int j=0;j<n;j++){
+    cout<<"\t"<<adj_mat[i][j]<<"\t";
+  }
+}
+
+time_t begin,end;
+double time_req;
+time (& begin);
+time (& end);
+time_req=difftime(end,begin);
+cout<<"Time required is: "<<time_req;
+return 0;
+}
 
 
 
